a19d8d86de5b9c3c18f0a7969ec870cabe6bbdfd
Kostiantyn Perun <kosperun@gmail.com>
2022-08-16 09:19:27 +0200

    Add addiotional tests for UserLoginAPIView

--- src/tests/api/test_user_views.py
+++ src/tests/api/test_user_views.py
@@ -1,13 +1,13 @@
 import pytest
 from django.urls import reverse
 from factory import Faker
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED
 
 from tests.domain.users.factories import UserFactory
 
 
 @pytest.mark.django_db
-def test_users_login(api_client, user):
+def test_user_login_with_fixtures_succeeds(api_client, user):
     url = reverse("users:user-login")  # "/api/users/login/"
     payload = {"email": user.email, "password": "1234"}
     response = api_client.post(url, payload)
@@ -16,10 +16,9 @@ def test_users_login(api_client, user):
 
 
 @pytest.mark.django_db
-def test_user_login(api_client):
+def test_user_login_succeeds(api_client):
     password = Faker("password")
     user = UserFactory(password=password)
-    print(user.password)
     user.set_password(str(password))
     user.save()
     url = reverse("users:user-login")  # "/api/users/login/"
@@ -27,3 +26,30 @@ def test_user_login(api_client):
     response = api_client.post(url, payload)
 
     assert response.status_code == HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_user_login_with_wrong_email_fails(api_client):
+    password = Faker("password")
+    user = UserFactory(password=password)
+    user.set_password(str(password))
+    user.save()
+    url = reverse("users:user-login")  # "/api/users/login/"
+    payload = {"email": Faker("email"), "password": password}
+    response = api_client.post(url, payload)
+
+    assert response.status_code == HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.django_db
+def test_user_login_with_wrong_password_fails(api_client):
+    password = Faker("password")
+    wrong_password = Faker("password")
+    user = UserFactory(password=password)
+    user.set_password(str(password))
+    user.save()
+    url = reverse("users:user-login")  # "/api/users/login/"
+    payload = {"email": user.email, "password": wrong_password}
+    response = api_client.post(url, payload)
+
+    assert response.status_code == HTTP_401_UNAUTHORIZED
