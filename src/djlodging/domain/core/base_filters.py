153827c5261350a35ba5a445fbb7c8bc9a9ee156
Kostiantyn Perun <kosperun@gmail.com>
2022-10-25 20:15:01 +0200

    Refactor BaseFilter

--- src/djlodging/domain/core/base_filters.py
+++ src/djlodging/domain/core/base_filters.py
@@ -5,9 +5,9 @@ from django_filters.filterset import FilterSetMetaclass
 
 
 class Filter:
-    def __init__(self, filter_set: FilterSetMetaclass) -> None:
-        self._filter_set_class = filter_set
+    def __init__(self, filterset: FilterSetMetaclass) -> None:
+        self._filterset_class = filterset
 
     def filter(self, query_params: dict, queryset: Optional[QuerySet] = None) -> QuerySet:
-        fs = self._filter_set_class(query_params, queryset=queryset)
+        fs = self._filterset_class(query_params, queryset=queryset)
         return fs.qs
