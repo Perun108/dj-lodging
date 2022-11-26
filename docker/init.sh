3b072aad256edb1c5c32ffbae589829792bd4963
Kostiantyn Perun <kosperun@gmail.com>
2022-11-25 20:47:05 -0500

    Modify dockefile

--- docker/init.sh
+++ docker/init.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-set -o nounset
+# set -o nounset
 
-python djlodging/django_app/manage.py collectstatic
-python djlodging/django_app/manage.py migrate
-nginx
+python djlodging/django_app/manage.py collectstatic --noinput
+# python djlodging/django_app/manage.py migrate
+# nginx
+# nginx -g "daemon off;"
 
-exec "$@"
+(gunicorn --bind 0.0.0.0:8010 djlodging.django_app.django_core.wsgi --workers 3) & nginx -g "daemon off;"
+# exec "$@"
