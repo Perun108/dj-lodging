9111ab8bbef6fd7e33c81f7599c84ef39e658a92
Kostiantyn Perun <kosperun@gmail.com>
2022-12-22 15:18:01 -0500

    Refactor email provider

--- src/djlodging/infrastructure/providers/email/__init__.py
+++ src/djlodging/infrastructure/providers/email/__init__.py
@@ -1,9 +1,13 @@
+from typing import Union
+
 from djlodging.infrastructure.providers.provider_injector import get_provider
 
 from .base_email_provider import BaseEmailProvider
 from .sendgrid import SendgridEmailProvider
 
-email_provider = get_provider("EMAIL_PROVIDER")
+email_provider_type = Union[BaseEmailProvider, SendgridEmailProvider]
+
+email_provider: email_provider_type = get_provider("EMAIL_PROVIDER")
 
 __all__ = [
     "email_provider",
