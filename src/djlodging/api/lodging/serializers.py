fc842ef714f21a713554cce77a7b7400686e1bdb
Kostiantyn Perun <kosperun@gmail.com>
2022-12-22 15:08:19 -0500

    Add docstrings

--- src/djlodging/api/lodging/serializers.py
+++ src/djlodging/api/lodging/serializers.py
@@ -1,23 +1,33 @@
+"""Module for all serializers related to the lodgings APIs."""
+
 from rest_framework import serializers
 
 from djlodging.api.users.serializers import LodgingUserOutputSerializer
 
 
 class CountryCreateInputSerializer(serializers.Serializer):
+    """Serializer to add a new country."""
+
     name = serializers.CharField()
 
 
 class CountryOutputSerializer(serializers.Serializer):
+    """Serializer to display a country's details."""
+
     id = serializers.UUIDField()
     name = serializers.CharField()
 
 
 class CountryPaginatedOutputSerializer(serializers.Serializer):
+    """Serializer to display a paginated list of countries."""
+
     count = serializers.IntegerField()
     results = CountryOutputSerializer(many=True)
 
 
 class CountryUpdateInputSerializer(serializers.Serializer):
+    """Serializer to accept input to update a country's details."""
+
     name = serializers.CharField()
 
 
